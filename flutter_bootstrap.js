b8a85aa3179b2c0a191942c53d19386581b3f78d
dimasdelonge-alt <dimas.delonge@gmail.com>
2026-02-27 14:30:44 +0000

    Deploying to gh-pages from @ dimasdelonge-alt/data-grooming@1b901aef6b2ba5aea5d01a79b56cdcfd4e38a8bb 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1522241332"
+    serviceWorkerVersion: "2483712101"
   }
 });
